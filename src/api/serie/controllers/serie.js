8169ffddda8a3ab2fb467e7e4a072ec08e1bf4bc
Mayte González <mayte710@hotmail.com>
2022-05-16 10:51:55 +0100

    backend

---
+++ src/api/serie/controllers/serie.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ *  serie controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::serie.serie", ({ strapi }) => ({
+  async update(ctx) {
+    const currentUserId = ctx.state.user.id;
+    ctx.request.body.data.user = currentUserId;
+    const { data, meta } = await super.update(ctx);
+    return { data, meta };
+  },
+  async create(ctx) {
+    const currentUserId = ctx.state.user.id;
+    ctx.request.body.data.user = currentUserId;
+    const { data, meta } = await super.create(ctx);
+    return { data, meta };
+  },
+}));
